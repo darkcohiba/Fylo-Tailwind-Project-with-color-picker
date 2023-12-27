863d6110200598f4216729ba5655378c04253a0c
Sam Waters <samuel.waters@gmail.com>
2023-12-27 15:58:03 -0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./dist/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkBlue: '',
+        darkBlue1:'',
+        darkBlue2:'',
+        darkBlue3:'',
+        accentCyan:'',
+        accentBlue:'',
+        lightRed:'',
+      },
+      fontFamily:{
+        sans: ['Raleway', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
