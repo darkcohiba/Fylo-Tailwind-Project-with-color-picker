2016a23e65a37c2bf5df0bc31ca24479576579d0
Sam Waters <samuel.waters@gmail.com>
2023-12-28 20:19:02 -0700

    chapter: Header & Dark Mode Button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.{html,js}'],
   darkMode: 'class',
+  content: ['./dist/*.{html,js}'],
   theme: {
     extend: {
       colors:{
-        darkBlue: '',
-        darkBlue1:'',
-        darkBlue2:'',
-        darkBlue3:'',
-        accentCyan:'',
-        accentBlue:'',
-        lightRed:'',
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1:'hsl(218, 28%, 13%)',
+        darkBlue2:'hsl(216, 53%, 9%)',
+        darkBlue3:'hsl(219, 30%, 18%)',
+        accentCyan:'hsl(176, 68%, 64%)',
+        accentBlue:'hsl(198, 60%, 50%)',
+        lightRed:'hsl(0, 100%, 63%)',
       },
       fontFamily:{
         sans: ['Raleway', 'sans-serif'],
